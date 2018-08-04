d00fb3dd0a8634c184fe8ebf92a48705b6b2b0bd
luahua <luyuhua6@126.com>
2018-08-04 19:04:07 +0800

    2

--- Controllers/SpeechRecognitionController.cs
+++ Controllers/SpeechRecognitionController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
-using System.IO;
+using System.IO.Pipes;
 using Baidu.Aip;
 
 namespace MvcMovie.Controllers
@@ -17,10 +17,10 @@ namespace MvcMovie.Controllers
             var API_KEY = "你的 Api Key";
             var SECRET_KEY = "你的 Secret Key";
             var client = new Baidu.Aip.Speech.Asr(APP_ID,API_KEY,SECRET_KEY);
-            var data = File.ReadAllBytes("语音pcm文件地址");
+            //var data = File.ReadAllBytes("语音pcm文件地址");
+            var data = System.IO.File.ReadAllBytes("");
             client.Timeout = 120000; // 若语音较长，建议设置更大的超时时间. ms
             var result = client.Recognize(data, "pcm", 16000);
-            Console.Write(result);
             return "";
         }
 
